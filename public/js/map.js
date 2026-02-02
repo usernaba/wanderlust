86e7331321df046ffa9500e3e4c8f9d31c06803c
usernaba <ntudu7672@gmail.com>
2026-02-02 15:26:15 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,14 @@
+maptilersdk.config.apiKey = mapToken;
+
+const map = new maptilersdk.Map({
+    container: 'map',
+    style: maptilersdk.MapStyle.STREETS,
+    center: listing.geometry.coordinates,
+    zoom: 9,
+});
+
+
+const marker = new maptilersdk.Marker({color: "red"})
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new maptilersdk.Popup({ offset: 25 }).setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking!</p>`))
+    .addTo(map);
---
+++ utils/ExpressError.js
@@ -0,0 +1,16 @@
+class ExpressError extends Error {
+    constructor(statusCode, message) {
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+
+module.exports = ExpressError;
+
+// class ExpressError extends Error {
+//     constructor(statusCode, message) {
+//         super(message);
+//         this.statusCode = statusCode;
+//     }
+// }
