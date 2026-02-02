1f8a60d52cdd6d689ceb2b2b0382a1d7a94e4b36
usernaba <ntudu7672@gmail.com>
2026-02-02 18:24:29 +0530

    Change root route to redirect to listings

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ const sessionOptions = {
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {   
+    res.redirect(`/listings`)
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
